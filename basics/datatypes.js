6a1b76c879e789c7901434788bfcc83267bb9060
navneet039 <2023ugec039@nitjsr.ac.in>
2025-05-18 08:36:59 +0000

    basics completed

--- basics/datatypes.js
+++ basics/datatypes.js
@@ -54,3 +54,27 @@ let number=33
 let stringNumber=String(number)
 console.log(stringNumber)
 console.log(typeof stringNumber)
+
+
+
+// ------>  operations----->
+
+let value=3
+let negval=-value
+console.log(negval)
+//  ** exponential  % modulo
+
+let str1="hello"
+
+let str2=" navneet"
+let str3=str1+str2
+console.log(str3)
+
+console.log("1"+"2")//12
+console.log(1+"2")//12
+console.log("1"+2+2)//122
+console.log(1+2+"2")//32
+
+
+console.log(+true)
+console.log(+"")
