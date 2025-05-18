d7e04670260dc8cd853d4bcadd2ff32cf6fce73a
navneet039 <2023ugec039@nitjsr.ac.in>
2025-05-18 08:37:23 +0000

    summary of data types

--- basics/datatypes.js
+++ basics/datatypes.js
@@ -78,3 +78,27 @@ console.log(1+2+"2")//32
 
 console.log(+true)
 console.log(+"")
+
+
+//----->comparision---->
+
+// < <=  > >= != 
+
+
+console.log("2" >1)
+console.log("02" > 1 )
+
+console.log(null > 0)
+console.log(null >= 0)
+console.log(null == 0)
+//the reason is that an equality check == and comaprision > < >= <= works differntely .
+//comparisions converts null to a number treating it as 0. that is why (92) null>=0 is true and null>0 is false
+
+console.log(undefined==0)
+console.log(undefined>=0)
+console.log(undefined<=0)
+
+
+// strict check === datatypes v check 
+
+console.log("2"===2)
---
+++ basics/summary.js
@@ -0,0 +1,38 @@
+//primitive and non primitive---->on the basis of data ko kis trh se memory me rkhte hai 
+
+
+//primitive --->string  Number ,Boolean ,null,undefined ,Symbol,BigInt
+
+
+// is javascript statically or dynamically typed---->js is dynamically typed language .this means 
+//variables types are determined at runtime and you donot need to explicitly declare the type of variable before using it
+
+
+const id=Symbol('123')//-->typeof is symbol
+const anotherid=Symbol('123')
+// ----->symbol also gives a returned value   it also of type symbol
+console.log(id===anotherid)
+const bignumber=887737274368184732676736n //bigint
+
+//Reference type --->
+
+//---->Arrays,Objects,----->typeof is objects---->Functions--->type is function object of objects
+
+const heros=["shaktiman","nagraj"]
+
+let info={
+    name:"navneet",
+    age:22
+}
+
+const myfn=function(){
+    console.log("hello world")
+
+}
+
+
+console.log(typeof myfn)
+console.log(typeof undefined)
+console.log(typeof heros)
+console.log(typeof id)
+
